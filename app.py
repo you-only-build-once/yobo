99502a99771eb9f1e03e3b2c0cbd562f221fa14f
joshuadaic0909 <103535145+joshuadaic0909@users.noreply.github.com>
2023-07-15 15:47:39 -0700

    update app.py

--- app.py
+++ app.py
@@ -4,22 +4,6 @@ from function import uml
 
 import urllib.request
 
-# import requests
-# from PIL import Image
-# import io
-# import base64
-
-# def get_image(url):
-#     response = requests.get(url)
-#     image = Image.open(io.BytesIO(response.content))
-#     return image
-
-# def get_image_download_link(img, filename='download_image.png', text='Click here to download: '):
-#     buffered = io.BytesIO()
-#     img.save(buffered, format="PNG")
-#     img_str = base64.b64encode(buffered.getvalue()).decode()
-#     href = f'<a href="data:file/png;base64,{img_str}" download="{filename}">{text}{filename}</a>'
-#     return href
 
 # page config 
 st.set_page_config(
@@ -29,25 +13,26 @@ st.set_page_config(
     initial_sidebar_state="expanded",
 )
 
-hide_st_style = """
-            <style>
-            MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            header {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_st_style, unsafe_allow_html=True)
+# hide_st_style = """
+#             <style>
+#             MainMenu {visibility: hidden;}
+#             footer {visibility: hidden;}
+#             header {visibility: hidden;}
+#             </style>
+#             """
+# st.markdown(hide_st_style, unsafe_allow_html=True)
 
 # page header 
 st.markdown(
     """
     <h1 style='text-align: center;'>YOBO.io</h1>
     <h5 style='text-align: center;'>From UML Design to Repo Generation</h5>
+    <h5 style='text-align: center;'><span style='font-size: 16px;'><a href="https://github.com/you-only-build-once/yobo">https://github.com/you-only-build-once/yobo</a></span></h5>
     """,
     unsafe_allow_html=True
 )
 
-ui_block, uml_block = st.columns([1, 1])
+ui_block, uml_block, folder_struct_block = st.columns([1, 1, 1])
 with ui_block:
     uml_framework_req = st.text_input('Preferred framework structure'
                                       , help = 'please input a rough preferences of your framework structure '
@@ -55,7 +40,7 @@ with ui_block:
     uml_project_req = st.text_area("Tell me about your project"
                         , help = 'please include the description, key features, functionalitiies of your project'
                         , placeholder = 'meeting note summarize app using ... '
-                        , height = 500)
+                        , height = 450)
     
     submit_button = st.button("Submit")
     if submit_button:
@@ -71,5 +56,5 @@ with uml_block:
             unsafe_allow_html=True
         )
     except NameError:
-        st.write("waiting for user description")
+        st.write("waiting for user description...")
     
