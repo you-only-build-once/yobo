aa25b86c5ecee7441a52b8c6f22693ef2e463e59
Shreyas Agarwal <shreyas1230@hotmail.com>
2023-07-15 17:36:18 -0700

    Fixed folder structure gen retries

--- function/folder_structre_gen.py
+++ function/folder_structre_gen.py
@@ -46,10 +46,14 @@ def folder_structure_gen(problem_description: str, uml_code: str, max_retries: i
             function_call = output.get("function_call", None)
 
             if function_call is None:
-                return None
+                retries += 1
+                codegen_agent.logger.warning("ChatGPT response unsuficient. Retrying...")
+                pass
 
             if function_call["name"] != "submit_file_structure":
-                return None
+                retries += 1
+                codegen_agent.logger.warning("ChatGPT response unsuficient. Retrying...")
+                pass
             else:
                 arguments = function_call["arguments"]
                 out_folder = json.loads(arguments)
