9dd6d11423b7702c827a104802c55721c0a1d1b9
santiag0m <32421689+santiag0m@users.noreply.github.com>
2023-07-15 18:59:29 -0700

    Code gen

--- test_bala.py
+++ test_bala.py
@@ -2,14 +2,17 @@ import json
 from function.uml import generate_uml_code
 from function.folder_structre_gen import folder_structure_gen, download_folder_structure
 from function.file_gen import codegen
+from function.endpoint_gen import endpoint_generation
 
 problem = "create a language model that summarizes a meeting from transcripts and get the keypoints out of it."
 out = generate_uml_code(problem, framework_requirements="FastAPI")
 folder_structure = folder_structure_gen(problem, out["uml_code"])
 
+
 # folder_structure = out_folder["file_structure"]
 # print(folder_structure)
 folder_text = json.dumps(folder_structure)
+endpoints = endpoint_generation(problem, out["uml_code"], folder_structure)
 # out_file = codegen(problem, out["uml_code"], folder_text)
 
 
